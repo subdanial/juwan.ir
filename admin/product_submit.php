b05c59f36df9bdbcb401e193b062fcf1a979a1de
subdanial <subdanial@gmail.com>
2020-04-12 20:10:41 +0430

    uploadrand

--- admin/product_submit.php
+++ admin/product_submit.php
@@ -37,7 +37,7 @@ for( $i=0 ; $i < $total ; $i++ ) {
   $tmpFilePath = $_FILES['image']['tmp_name'][$i];
 
   if ($tmpFilePath != ""){
-    $newFilePath = "../uploads/img/" . $_FILES['image']['name'][$i];
+    $newFilePath = "../uploads/img/" . rand(100000,10000000).$_FILES['image']['name'][$i];
     if(move_uploaded_file($tmpFilePath, $newFilePath)) {
       array_push($images_parameter,"\"http:\\".$_SERVER['HTTP_HOST']."/".explode("..",$newFilePath)[1]."\"");
     }
