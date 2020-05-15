b1331bf8eab839a3841a9cc94dc353c4d774c278
subdanial <subdanial@gmail.com>
2020-05-15 16:30:19 +0430

    pagination

--- category.php
+++ category.php
@@ -288,7 +288,7 @@ $products =  $database->products_show($cat_id,$colors,$materials,$styles,$brands
                         </div>
                         <?php  $i++;endforeach; ?>
                         <!-- catalog end -->
-                        <nav class="position-absolute d-block float-right" style="bottom:0px;left:0">
+                        <nav class="d-block float-right">
                             <ul class="pagination">
 
                                 <?php  for ($page=1;$page<=$number_of_pages;$page++) :?>
